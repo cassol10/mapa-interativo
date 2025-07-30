4b1b525722195a34da134a7cd794fea63661c1d6
cassol10 <andecassol@gmail.com>
2025-07-30 23:13:43 +0100

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbUVgbofQ-e4e_umFYIbB48ePjBGHLz3g",
+  authDomain: "mapa-interativo-88475.firebaseapp.com",
+  databaseURL: "https://mapa-interativo-88475-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mapa-interativo-88475",
+  storageBucket: "mapa-interativo-88475.firebasestorage.app",
+  messagingSenderId: "331142352241",
+  appId: "1:331142352241:web:4c0319be9b0f5c7a113ad0",
+  measurementId: "G-7FFZ0Q53M0"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
